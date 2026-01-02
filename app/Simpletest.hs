fe74abd5d91e2dfbc91466a7b2238778a2d84038
Brian Johnson <bjthinks@gmail.com>
2026-01-01 18:48:42 -0800

    Compute sylow numbers

--- app/Simpletest.hs
+++ app/Simpletest.hs
@@ -20,17 +20,31 @@ computeMinIndex n k
   | k `mod` n == 0 = k
   | otherwise = computeMinIndex (n `div` (gcd n k)) (k+1)
 
+sylowNumbers :: Int -> [(Int,Int)] -> [[Int]]
+sylowNumbers g factors = map (sylowNumber g) factors
+
+sylowNumber :: Int -> (Int,Int) -> [Int]
+sylowNumber g (p,e) =
+  do let pPart = p ^ e
+         pPrimePart = g `div` pPart
+     candidate <- [1..pPrimePart]
+     if g `mod` candidate == 0 && candidate `mod` p == 1
+       then return candidate
+       else []
+
 simpletest :: Int -> MaybeT (Writer String) ()
-simpletest n = do
-  tell $ show n ++ "\t"
-  let factorization = trialDivide n
+simpletest g = do
+  tell $ show g ++ "\t"
+  let factorization = trialDivide g
   if length factorization == 1 && snd (head factorization) == 1
     then stop "prime number"
     else return ()
   if length factorization == 1
     then stop $ "power of " ++ show (fst (head factorization))
     else return ()
-  let minIndex = computeMinIndex n 3
+  let minIndex = computeMinIndex g 3
+      sylow = sylowNumbers g factorization
+  tell $ show sylow
   nl
 
 main :: IO ()
